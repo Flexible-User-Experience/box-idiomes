4c4c8731c1d229ed49f113bf5640504a3c4529cd
Anton Serra <aserratorta@gmail.com>
2016-12-21 16:26:40 +0100

    create teacher repository

---
+++ src/AppBundle/Repository/TeacherRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class TeacherRepository
+ *
+ * @category Repository
+ * @package  AppBundle\Repository
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class TeacherRepository extends EntityRepository
+{
+
+}
