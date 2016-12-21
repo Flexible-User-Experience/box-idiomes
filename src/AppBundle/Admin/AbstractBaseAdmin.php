80a930c6fe1b7f257a9ad2c4b7c060a57ecc30a0
Anton Serra <aserratorta@gmail.com>
2016-12-21 17:34:37 +0100

    create teacher admin and render

---
+++ src/AppBundle/Admin/AbstractBaseAdmin.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use AppBundle\Manager\RepositoriesManager;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+
+/**
+ * Class BaseAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   David Romaní <david@flux.cat>
+ */
+abstract class AbstractBaseAdmin extends AbstractAdmin
+{
+//    /**
+//     * @var UploaderHelper
+//     */
+//    private $vus;
+//
+//    /**
+//     * @var CacheManager
+//     */
+//    private $lis;
+
+//    /**
+//     * @param string              $code
+//     * @param string              $class
+//     * @param string              $baseControllerName
+//     * @param UploaderHelper      $vus
+//     * @param CacheManager        $lis
+//     */
+//    public function __construct($code, $class, $baseControllerName, UploaderHelper $vus, CacheManager $lis)
+//    {
+//        parent::__construct($code, $class, $baseControllerName);
+//        $this->vus = $vus;
+//        $this->lis = $lis;
+//    }
+
+    /**
+     * @var array
+     */
+    protected $perPageOptions = array(25, 50, 100, 200);
+
+    /**
+     * @var int
+     */
+    protected $maxPerPage = 25;
+
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->remove('show')
+            ->remove('batch');
+    }
+
+    /**
+     * Remove batch action list view first column
+     *
+     * @return array
+     */
+    public function getBatchActions()
+    {
+        $actions = parent::getBatchActions();
+        unset($actions['delete']);
+
+        return $actions;
+    }
+
+    /**
+     * Get export formats
+     *
+     * @return array
+     */
+    public function getExportFormats()
+    {
+        return array(
+            'csv',
+            'xls',
+        );
+    }
+
+    /**
+     * @param string $bootstrapGrid
+     * @param string $bootstrapSize
+     * @param string $boxClass
+     *
+     * @return array
+     */
+    protected function getDefaultFormBoxArray($bootstrapGrid = 'md', $bootstrapSize = '6', $boxClass = 'primary')
+    {
+        return array(
+            'class'     => 'col-' . $bootstrapGrid . '-' . $bootstrapSize,
+            'box_class' => 'box box-' . $boxClass,
+        );
+    }
+
+    /**
+     * @param string $bootstrapColSize
+     *
+     * @return array
+     */
+    protected function getFormMdSuccessBoxArray($bootstrapColSize = '6')
+    {
+        return $this->getDefaultFormBoxArray('md', $bootstrapColSize, 'success');
+    }
+
+    /**
+     * Get image helper form mapper with thumbnail
+     *
+     * @return string
+     */
+    protected function getImageHelperFormMapperWithThumbnail()
+    {
+        return ($this->getSubject() ? $this->getSubject()->getImageName() ? '<img src="' . $this->lis->getBrowserPath(
+                $this->vus->asset($this->getSubject(), 'imageFile'),
+                '480xY'
+            ) . '" class="admin-preview img-responsive" alt="thumbnail"/>' : '' : '') . '<span style="width:100%;display:block;">amplada mínima 1200px (màx. 10MB amb JPG o PNG)</span>';
+    }
+
+    /**
+     * Get image helper form mapper with thumbnail for black&white
+     *
+     * @return string
+     */
+    protected function getImageHelperFormMapperWithThumbnailBW()
+    {
+        return ($this->getSubject() ? $this->getSubject()->getImageNameBW() ? '<img src="' . $this->lis->getBrowserPath(
+                $this->vus->asset($this->getSubject(), 'imageFileBW'),
+                '480xY'
+            ) . '" class="admin-preview img-responsive" alt="thumbnail"/>' : '' : '') . '<span style="width:100%;display:block;">amplada mínima 1200px (màx. 10MB amb JPG o PNG)</span>';
+    }
+
+    protected function getImageHelperFormMapperWithThumbnailGif()
+    {
+        return ($this->getSubject() ? $this->getSubject()->getGifName() ? '<img src="' . $this->lis->getBrowserPath(
+                $this->vus->asset($this->getSubject(), 'gifFile'),
+                '480xY'
+            ) . '" class="admin-preview img-responsive" alt="thumbnail"/>' : '' : '') . '<span style="width:100%;display:block;">mida 780x1168px (màx. 10MB amb GIF)</span>';
+    }
+}
---
+++ src/AppBundle/Admin/TeacherAdmin.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+/**
+ * Class TeacherAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class TeacherAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Teacher';
+    protected $baseRoutePattern = 'teachers/teacher';
+    protected $datagridValues = array(
+        '_sort_by'    => 'name',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->remove('batch');
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', $this->getFormMdSuccessBoxArray(6))
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'Nom',
+                )
+            )
+            ->end()
+            ->with('Controls', $this->getFormMdSuccessBoxArray(6))
+            ->add(
+                'enabled',
+                'checkbox',
+                array(
+                    'label'    => 'Actiu',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'Nom',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'Actiu',
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label'    => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            );
+//            ->add(
+//                '_action',
+//                'actions',
+//                array(
+//                    'actions' => array(
+//                        'show'   => array(
+//                            'template' => '::Admin/Buttons/list__action_show_button.html.twig'
+//                        ),
+//                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+//                        'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
+//                    ),
+//                    'label'   => 'backend.admin.actions',
+//                )
+//            );
+    }
+}
