f485cd46a225bb626799d34f3988b23f2660705a
Anton Serra <aserratorta@gmail.com>
2016-12-27 16:50:45 +0100

    create teachers, services and contact controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,4 +17,38 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
+
+    /**
+     * @Route("/professors", name="app_teachers")
+     */
+    public function teachersAction()
+    {
+        return $this->render(
+            ':Front/teachers.html.twig'
+//            ['teachers' => $teachers]
+        );
+    }
+
+
+    /**
+     * @Route("/serveis", name="app_services")
+     */
+    public function servicesAction()
+    {
+        return $this->render(
+            ':Front/services.html.twig'
+//            ['teachers' => $teachers]
+        );
+    }
+
+    /**
+     * @Route("/contacte", name="app_contact")
+     */
+    public function contactAction()
+    {
+        return $this->render(
+            ':Front/contact.html.twig'
+//            ['teachers' => $teachers]
+        );
+    }
 }
