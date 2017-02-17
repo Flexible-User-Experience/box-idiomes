d8c25a291e98219e880baa712020eba601a37921
Anton Serra <aserratorta@gmail.com>
2017-02-18 00:05:39 +0100

    create NewsletterContact entity

---
+++ src/AppBundle/Repository/NewsletterContactRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class NewsletterContactRepository
+ *
+ * @category Repository
+ * @package  AppBundle\Repository
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class NewsletterContactRepository extends EntityRepository
+{
+
+}
