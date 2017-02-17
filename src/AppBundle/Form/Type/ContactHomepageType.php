ab9e7f2f1f021b0720df2dcdde8ea41800d0c751
Anton Serra <aserratorta@gmail.com>
2017-02-18 00:29:52 +0100

    match newsletter entity to form

--- src/AppBundle/Form/Type/ContactHomepageType.php
+++ src/AppBundle/Form/Type/ContactHomepageType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -61,5 +62,17 @@ class ContactHomepageType extends AbstractType
     public function getBlockPrefix()
     {
         return 'contact_homepage';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'AppBundle\Entity\NewsletterContact',
+            )
+        );
     }
 }
