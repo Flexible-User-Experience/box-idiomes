1f49293623fb93210affcac6bd7bf1b814aaebf5
Wils Iglesias <wiglesias83@gmail.com>
2017-10-23 11:33:38 +0200

    add image rights pdf button in student list view

--- src/AppBundle/Admin/StudentAdmin.php
+++ src/AppBundle/Admin/StudentAdmin.php
@@ -290,6 +290,9 @@ class StudentAdmin extends AbstractBaseAdmin
                 array(
                     'actions' => array(
                         'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                        'imagerights' => array(
+                            'template' => '::Admin/Cells/list__action_imager_ights.html.twig',
+                        ),
                         'delete' => array('template' => '::Admin/Buttons/list__action_delete_button.html.twig'),
                     ),
                     'label' => 'Accions',
