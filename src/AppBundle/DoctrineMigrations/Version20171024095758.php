56e1048291acf66f31f4351cf0c03ee37a380504
Wils Iglesias <wiglesias83@gmail.com>
2017-10-24 11:58:28 +0200

    new doctrine migration

---
+++ src/AppBundle/DoctrineMigrations/Version20171024095758.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171024095758 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE student ADD parent_address VARCHAR(255) DEFAULT NULL, ADD bank_account_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE student DROP parent_address, DROP bank_account_name');
+    }
+}
