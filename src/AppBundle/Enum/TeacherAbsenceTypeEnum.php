b5eb0dd638b436802856ee5c47aef3adc1a9172f
Wils Iglesias <wiglesias83@gmail.com>
2017-11-08 10:11:35 +0100

    add TeacherAbsenceTypeEnum

---
+++ src/AppBundle/Enum/TeacherAbsenceTypeEnum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Enum;
+
+/**
+ * Class TeacherAbsenceTypeEnum.
+ *
+ * @category Enum
+ *
+ * @author   Wils Iglesias <wiglesias83@gmail.com>
+ */
+class TeacherAbsenceTypeEnum
+{
+    const PERSONAL_ISSUES = 0;
+    const TRAINING = 1;
+    const OTHER_MOTIVES = 2;
+
+    /**
+     * @return array
+     */
+    public static function getEnumArray()
+    {
+        return array(
+            self::PERSONAL_ISSUES => 'Assumptes personals',
+            self::TRAINING => 'Formació',
+            self::OTHER_MOTIVES => 'Altres motius',
+        );
+    }
+}
