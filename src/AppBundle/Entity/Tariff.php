cbd5ca57056a4438ff11b4f70b40d5caa192465a
Wils Iglesias <wiglesias83@gmail.com>
2017-11-09 13:06:23 +0100

    add Tariff entity

---
+++ src/AppBundle/Entity/Tariff.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Tariff.
+ *
+ * @category Entity
+ *
+ * @author   Wils Iglesias <wiglesias83@gmail.com>
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="tariff")
+ */
+class Tariff extends AbstractBase
+{
+}
