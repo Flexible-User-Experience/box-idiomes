2fced0fcf43f1b9323ebe9cc1250ea429f0fef18
Wils Iglesias <wiglesias83@gmail.com>
2017-11-10 10:16:50 +0100

    new doctrine migration

--- src/AppBundle/Entity/Tariff.php
+++ src/AppBundle/Entity/Tariff.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Class Tariff.
@@ -11,9 +12,121 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @author   Wils Iglesias <wiglesias83@gmail.com>
  *
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TariffRepository")
  * @ORM\Table(name="tariff")
+ * @UniqueEntity({})
  */
 class Tariff extends AbstractBase
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer", options={"default"=0})
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $name;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int $year
+     *
+     * @return Tariff
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     *
+     * @return Tariff
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     *
+     * @return Tariff
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Tariff
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
