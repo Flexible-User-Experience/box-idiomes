910855e22e59e119aa1f2ebab31426639a2e2494
Wils Iglesias <wiglesias83@gmail.com>
2017-11-10 11:10:48 +0100

    add Tariff list admin

--- src/AppBundle/Admin/TariffAdmin.php
+++ src/AppBundle/Admin/TariffAdmin.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Admin;
 
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
 /**
@@ -16,7 +17,7 @@ class TariffAdmin extends AbstractBaseAdmin
     protected $classnameLabel = 'Tarriff';
     protected $baseRoutePattern = 'classrooms/tariff';
     protected $datagridValues = array(
-        '_sort_by' => 'surname',
+        '_sort_by' => 'year',
         '_sort_order' => 'asc',
     );
 
@@ -37,6 +38,66 @@ class TariffAdmin extends AbstractBaseAdmin
         parent::configureRoutes($collection);
         $collection
             ->remove('delete')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'year',
+                null,
+                array(
+                    'label' => 'backend.admin.tariff.year',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'price',
+                null,
+                array(
+                    'label' => 'backend.admin.tariff.price',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'type',
+                null,
+                array(
+                    'label' => 'backend.admin.tariff.type',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'backend.admin.tariff.name',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                    ),
+                    'label' => 'backend.admin.actions',
+                )
+            )
         ;
     }
 }
