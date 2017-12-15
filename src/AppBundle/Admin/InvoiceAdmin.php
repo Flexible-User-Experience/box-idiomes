2260d09c16e05f2f4c014c8402616e656b6bcf17
Wils Iglesias <wiglesias83@gmail.com>
2017-12-15 18:12:09 +0100

    add invoice list admin

--- src/AppBundle/Admin/InvoiceAdmin.php
+++ src/AppBundle/Admin/InvoiceAdmin.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Admin;
 
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
 /**
@@ -30,5 +31,82 @@ class InvoiceAdmin extends AbstractBaseAdmin
         parent::configureRoutes($collection);
         $collection
             ->remove('delete');
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'id',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.id',
+                    'editable' => false,
+                )
+            )
+            ->add(
+                'date',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.date',
+                    'editable' => true,
+                    'format' => 'd/m/Y',
+                )
+            )
+            ->add(
+                'student',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.student',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'person',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.person',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'baseAmount',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.baseAmount',
+                    'editable' => false,
+                )
+            )
+            ->add(
+                'taxParcentage',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.taxParcentage',
+                    'editable' => false,
+                )
+            )
+            ->add(
+                'totalAmount',
+                null,
+                array(
+                    'label' => 'backend.admin.invoice.totalAmount',
+                    'editable' => false,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'edit' => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                    ),
+                    'label' => 'backend.admin.actions',
+                )
+            )
+        ;
     }
 }
