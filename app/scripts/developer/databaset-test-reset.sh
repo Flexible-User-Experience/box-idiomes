c58f2ed7e78d740f527e48f714992e7e6b5d6c32
David Romaní <david@flux.cat>
2017-12-22 17:13:20 +0100

    include developer scripts

---
+++ app/scripts/developer/databaset-test-reset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+php app/console doctrine:database:drop --force --env=test
+php app/console doctrine:database:create --env=test
+php app/console doctrine:schema:update --force --env=test
+php app/console hautelook_alice:doctrine:fixtures:load -n --env=test
