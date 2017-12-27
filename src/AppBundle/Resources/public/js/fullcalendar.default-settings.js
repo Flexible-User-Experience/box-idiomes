1b4fdb206d5aed5c7ddba6bc3f6b654ce6309f31
David Romaní <david@flux.cat>
2017-12-27 16:01:36 +0100

    improve fullcalendar hours displayed

--- src/AppBundle/Resources/public/js/fullcalendar.default-settings.js
+++ src/AppBundle/Resources/public/js/fullcalendar.default-settings.js
@@ -1,9 +1,21 @@
 jQuery(function () {
     jQuery('#calendar-holder').fullCalendar({
         header: {
-            left: 'prev,next,today',
+            left: 'prev,next today',
             center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
+            right: 'month,agendaWeek,agendaDay listWeek'
+        },
+        views: {
+            agendaWeek: {
+                allDaySlot: false,
+                minTime: '06:00:00',
+                maxTime: '22:00:00'
+            },
+            agendaDay: {
+                allDaySlot: false,
+                minTime: '06:00:00',
+                maxTime: '22:00:00'
+            }
         },
         height: 600,
         locale: 'ca',
@@ -16,6 +28,7 @@ jQuery(function () {
         displayEventTime: true,
         weekNumbers: false,
         defaultView: 'agendaWeek',
+        themeSystem: 'bootstrap3',
         events: {
             url: Routing.generate('ancarebeca_full_calendar_load'),
             type: 'POST',
