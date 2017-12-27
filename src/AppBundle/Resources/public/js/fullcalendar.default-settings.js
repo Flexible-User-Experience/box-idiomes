d4680cc434374fe22ce90a6c002bbc2f1047b23a
David Romaní <david@flux.cat>
2017-12-27 17:58:15 +0100

    add gcal holiday events in fullcalendar

--- src/AppBundle/Resources/public/js/fullcalendar.default-settings.js
+++ src/AppBundle/Resources/public/js/fullcalendar.default-settings.js
@@ -1,23 +1,25 @@
 jQuery(function () {
     jQuery('#calendar-holder').fullCalendar({
         header: {
-            left: 'prev,next today',
+            left: 'prev today next',
             center: 'title',
             right: 'month,agendaWeek,agendaDay listWeek'
         },
         views: {
             agendaWeek: {
-                allDaySlot: false,
+                allDaySlot: true,
+                scrollTime: '08:00:00',
                 minTime: '06:00:00',
                 maxTime: '22:00:00'
             },
             agendaDay: {
-                allDaySlot: false,
+                allDaySlot: true,
+                scrollTime: '08:00:00',
                 minTime: '06:00:00',
                 maxTime: '22:00:00'
             }
         },
-        height: 600,
+        height: 750,
         locale: 'ca',
         firstDay: 1,
         lazyFetching: false,
@@ -29,13 +31,22 @@ jQuery(function () {
         weekNumbers: false,
         defaultView: 'agendaWeek',
         themeSystem: 'bootstrap3',
-        events: {
-            url: Routing.generate('ancarebeca_full_calendar_load'),
-            type: 'POST',
-            data: {},
-            error: function(data) {
-                console.log('error!', data.responseText);
+        googleCalendarApiKey: 'AIzaSyCZZYZV-LqX2qDtggiEo1GmeNhxe3SAhfI',
+        eventSources: [
+            {
+                googleCalendarId: 'es.spain#holiday@group.v.calendar.google.com',
+                backgroundColor: '#FED3D7',
+                textColor: '#FF0000',
+                color: '#FED3D7'
+            },
+            {
+                url: Routing.generate('ancarebeca_full_calendar_load'),
+                type: 'POST',
+                data: {},
+                error: function(data) {
+                    console.log('error!', data.responseText);
+                }
             }
-        }
+        ]
     });
 });
