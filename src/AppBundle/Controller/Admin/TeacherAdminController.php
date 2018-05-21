e6c624fdbfe15975b1b0d4a06980baf725de57a5
David Romaní <david@flux.cat>
2018-05-21 17:41:56 +0200

    improve teacher detail admin template 3

--- src/AppBundle/Controller/Admin/TeacherAdminController.php
+++ src/AppBundle/Controller/Admin/TeacherAdminController.php
@@ -38,11 +38,14 @@ class TeacherAdminController extends BaseAdminController
             throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
         }
 
+        $absences = $this->container->get('app.teacher_absence_repository')->getTeacherAbsencesSortedByDate($object);
+
         return $this->render(
             '::Admin/Teacher/detail.html.twig',
             array(
                 'action' => 'show',
                 'object' => $object,
+                'absences' => $absences,
                 'elements' => $this->admin->getShow(),
             )
         );
