bf79e46b70f5efcf23caec8a08a0dcec4848b29a
David Romaní <david@flux.cat>
2018-05-24 18:37:00 +0200

    improve admin invoice auto-generator step 1

--- src/AppBundle/Form/Type/GenerateInvoiceType.php
+++ src/AppBundle/Form/Type/GenerateInvoiceType.php
@@ -2,16 +2,21 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Enum\InvoiceYearMonthEnum;
+use AppBundle\Form\Model\GenerateInvoiceModel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class GenerateInvoiceType.
  *
  * @category FormType
  */
-class GenerateInvoiceType extends GenerateInvoiceYearMonthChooserType
+class GenerateInvoiceType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
@@ -21,19 +26,48 @@ class GenerateInvoiceType extends GenerateInvoiceYearMonthChooserType
     {
         parent::buildForm($builder, $options);
         $builder
-            ->remove('preview')
+            ->add(
+                'year',
+                ChoiceType::class,
+                array(
+                    'label' => 'backend.admin.invoice.year',
+                    'required' => true,
+                    'choices' => InvoiceYearMonthEnum::getYearEnumArray(),
+                    'choices_as_values' => true,
+                )
+            )
+            ->add(
+                'month',
+                ChoiceType::class,
+                array(
+                    'label' => 'backend.admin.invoice.month',
+                    'required' => true,
+                    'choices' => InvoiceYearMonthEnum::getMonthEnumArray(),
+                    'choices_as_values' => false,
+                )
+            )
             ->add(
                 'items',
                 CollectionType::class,
                 array(
                     'label' => 'backend.admin.invoice.items',
-                    'allow_extra_fields' => true,
+                    'allow_extra_fields' => false,
                     'cascade_validation' => true,
                     'required' => false,
                     'entry_type' => GenerateInvoiceItemType::class,
                     'by_reference' => false,
                     'entry_options' => array(
                         'label' => false,
+                    ),
+                )
+            )
+            ->add(
+                'preview',
+                SubmitType::class,
+                array(
+                    'label' => 'backend.admin.invoice.preview_invoice',
+                    'attr' => array(
+                        'class' => 'btn btn-success',
                     ),
                 )
             )
@@ -56,5 +90,17 @@ class GenerateInvoiceType extends GenerateInvoiceYearMonthChooserType
     public function getBlockPrefix()
     {
         return 'generate_invoice';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => GenerateInvoiceModel::class,
+            )
+        );
     }
 }
