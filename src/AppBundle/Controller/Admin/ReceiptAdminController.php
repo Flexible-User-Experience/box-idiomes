7c04af0811a9f88cdd1d30a29d4eb5b2fcc2f3a9
David Romaní <david@flux.cat>
2018-06-13 20:01:45 +0200

    improve ReceiptAdminController

--- src/AppBundle/Controller/Admin/ReceiptAdminController.php
+++ src/AppBundle/Controller/Admin/ReceiptAdminController.php
@@ -2,8 +2,9 @@
 
 namespace AppBundle\Controller\Admin;
 
-use AppBundle\Entity\Invoice;
+use AppBundle\Entity\Receipt;
 use AppBundle\Form\Model\GenerateReceiptModel;
+use AppBundle\Form\Type\GenerateReceiptType;
 use AppBundle\Form\Type\GenerateReceiptYearMonthChooserType;
 use AppBundle\Manager\GenerateReceiptFormManager;
 use AppBundle\Service\InvoicePdfBuilderService;
@@ -45,18 +46,18 @@ class ReceiptAdminController extends BaseAdminController
         $yearMonthForm->handleRequest($request);
 
         // build items form
-        $generateInvoice = new GenerateInvoiceModel();
+        $generateReceipt = new GenerateReceiptModel();
         /** @var Controller $this */
-        $form = $this->createForm(GenerateInvoiceType::class, $generateInvoice);
+        $form = $this->createForm(GenerateReceiptType::class, $generateReceipt);
         $form->handleRequest($request);
 
         if ($yearMonthForm->isSubmitted() && $yearMonthForm->isValid()) {
-            $year = $generateInvoiceYearMonthChooser->getYear();
-            $month = $generateInvoiceYearMonthChooser->getMonth();
+            $year = $generateReceiptYearMonthChooser->getYear();
+            $month = $generateReceiptYearMonthChooser->getMonth();
             // fill full items form
-            $generateInvoice = $gifm->buildFullModelForm($year, $month);
+            $generateReceipt = $grfm->buildFullModelForm($year, $month);
             /** @var Controller $this */
-            $form = $this->createForm(GenerateInvoiceType::class, $generateInvoice);
+            $form = $this->createForm(GenerateReceiptType::class, $generateReceipt);
         }
 
         return $this->renderWithExtraParams(
@@ -65,7 +66,7 @@ class ReceiptAdminController extends BaseAdminController
                 'action' => 'generate',
                 'year_month_form' => $yearMonthForm->createView(),
                 'form' => $form->createView(),
-                'generate_invoice' => $generateInvoice,
+                'generate_receipt' => $generateReceipt,
             )
         );
     }
@@ -84,10 +85,10 @@ class ReceiptAdminController extends BaseAdminController
      */
     public function creatorAction(Request $request = null)
     {
-        /** @var GenerateInvoiceFormManager $gifm */
-        $gifm = $this->container->get('app.generate_invoice_form_manager');
-        $generateInvoice = $gifm->transformRequestArrayToModel($request->get('generate_invoice'));
-        $recordsParsed = $gifm->persistFullModelForm($generateInvoice);
+        /** @var GenerateReceiptFormManager $grfm */
+        $grfm = $this->container->get('app.generate_receipt_form_manager');
+        $generateReceipt = $grfm->transformRequestArrayToModel($request->get('generate_receipt'));
+        $recordsParsed = $grfm->persistFullModelForm($generateReceipt);
 
         /** @var Translator $translator */
         $translator = $this->container->get('translator.default');
@@ -116,18 +117,18 @@ class ReceiptAdminController extends BaseAdminController
         $request = $this->resolveRequest($request);
         $id = $request->get($this->admin->getIdParameter());
 
-        /** @var Invoice $object */
+        /** @var Receipt $object */
         $object = $this->admin->getObject($id);
 
         if (!$object) {
             throw $this->createNotFoundException(sprintf('unable to find the object with id : %s', $id));
         }
 
-        /** @var InvoicePdfBuilderService $ips */
+        /** TODO @var InvoicePdfBuilderService $ips */
         $ips = $this->get('app.invoice_pdf_builder');
         $pdf = $ips->build($object);
 
-        return new Response($pdf->Output('box_idiomes_invoice_'.$object->getSluggedInvoiceNumber().'.pdf', 'I'), 200, array('Content-type' => 'application/pdf'));
+        return new Response($pdf->Output('box_idiomes_invoice_'.$object->getSluggedReceiptNumber().'.pdf', 'I'), 200, array('Content-type' => 'application/pdf'));
     }
 
     /**
@@ -146,7 +147,7 @@ class ReceiptAdminController extends BaseAdminController
         $request = $this->resolveRequest($request);
         $id = $request->get($this->admin->getIdParameter());
 
-        /** @var Invoice $object */
+        /** @var Receipt $object */
         $object = $this->admin->getObject($id);
 
         if (!$object) {
@@ -154,7 +155,7 @@ class ReceiptAdminController extends BaseAdminController
         }
 
         /* @var Controller $this */
-        $this->addFlash('danger', 'Aquesta funcionalitat encara no està disponible. No s\'ha enviat cap factura per email.');
+        $this->addFlash('danger', 'Aquesta funcionalitat encara no està disponible. No s\'ha enviat cap rebut per email.');
 
         return $this->redirectToList();
     }
