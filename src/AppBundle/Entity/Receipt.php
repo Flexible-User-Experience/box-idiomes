3b28de1fe713b05378f9a945c349eb0085dc0be7
David Romaní <david@flux.cat>
2018-06-13 12:17:29 +0200

    add new receipt entity & repository

---
+++ src/AppBundle/Entity/Receipt.php
@@ -0,0 +1,471 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Class Receipt.
+ *
+ * @category Entity
+ *
+ * @author   David Romaní <david@flux.cat>
+ *
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReceiptRepository")
+ * @ORM\Table(name="receipt")
+ * @UniqueEntity(fields={"month", "year", "student", "person"})
+ */
+class Receipt extends AbstractBase
+{
+    /**
+     * @var ArrayCollection|array|ReceiptLine[]
+     *
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\ReceiptLine", mappedBy="receipt", cascade={"persist", "remove"}, orphanRemoval=true)
+     */
+    private $lines;
+
+    /**
+     * @var Student
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Student")
+     * @ORM\JoinColumn(name="student_id", referencedColumnName="id")
+     */
+    private $student;
+
+    /**
+     * @var Person
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Person")
+     * @ORM\JoinColumn(name="person_id", referencedColumnName="id")
+     */
+    private $person;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isPayed;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $paymentDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isSended;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $sendDate;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $baseAmount;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $discountApplied;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $month;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * Invoice constructor.
+     */
+    public function __construct()
+    {
+        $this->lines = new ArrayCollection();
+    }
+
+    /**
+     * @return ReceiptLine[]|array|ArrayCollection
+     */
+    public function getLines()
+    {
+        return $this->lines;
+    }
+
+    /**
+     * @param ReceiptLine[]|array|ArrayCollection $lines
+     *
+     * @return $this
+     */
+    public function setLines($lines)
+    {
+        $this->lines = $lines;
+
+        return $this;
+    }
+
+    /**
+     * @param ReceiptLine $line
+     *
+     * @return $this
+     */
+    public function addLine(ReceiptLine $line)
+    {
+        if (!$this->lines->contains($line)) {
+            $line->setReceipt($this);
+            $this->lines->add($line);
+            $this->setBaseAmount($this->getBaseAmount() + $line->getTotal());
+            $this->setDiscountApplied($this->getStudent()->hasDiscount());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param ReceiptLine $line
+     *
+     * @return $this
+     */
+    public function removeLine(ReceiptLine $line)
+    {
+        if ($this->lines->contains($line)) {
+            $this->lines->removeElement($line);
+            $this->setBaseAmount($this->getBaseAmount() - $line->getTotal());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Student
+     */
+    public function getStudent()
+    {
+        return $this->student;
+    }
+
+    /**
+     * @param Student $student
+     *
+     * @return $this
+     */
+    public function setStudent($student)
+    {
+        $this->student = $student;
+
+        return $this;
+    }
+
+    /**
+     * @return Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person $person
+     *
+     * @return $this
+     */
+    public function setPerson($person)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateString()
+    {
+        return $this->date ? $this->date->format('d/m/Y') : '--/--/----';
+    }
+
+    /**
+     * @param \DateTime $date
+     *
+     * @return $this
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPayed()
+    {
+        return $this->isPayed;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsPayed()
+    {
+        return $this->isPayed();
+    }
+
+    /**
+     * @param bool $isPayed
+     *
+     * @return $this
+     */
+    public function setIsPayed($isPayed)
+    {
+        $this->isPayed = $isPayed;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getPaymentDate()
+    {
+        return $this->paymentDate;
+    }
+
+    /**
+     * @param \DateTime $paymentDate
+     *
+     * @return $this
+     */
+    public function setPaymentDate($paymentDate)
+    {
+        $this->paymentDate = $paymentDate;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSended()
+    {
+        return $this->isSended;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsSended()
+    {
+        return $this->isSended();
+    }
+
+    /**
+     * @param bool $isSended
+     *
+     * @return $this
+     */
+    public function setIsSended($isSended)
+    {
+        $this->isSended = $isSended;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getSendDate()
+    {
+        return $this->sendDate;
+    }
+
+    /**
+     * @param \DateTime $sendDate
+     *
+     * @return $this
+     */
+    public function setSendDate(\DateTime $sendDate)
+    {
+        $this->sendDate = $sendDate;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getBaseAmount()
+    {
+        return $this->baseAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseAmountString()
+    {
+        return number_format($this->baseAmount, 2, ',', '.').'€';
+    }
+
+    /**
+     * @param float $baseAmount
+     *
+     * @return $this
+     */
+    public function setBaseAmount($baseAmount)
+    {
+        $this->baseAmount = $baseAmount;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDiscountApplied()
+    {
+        return $this->discountApplied;
+    }
+
+    /**
+     * @param bool $discountApplied
+     *
+     * @return $this
+     */
+    public function setDiscountApplied($discountApplied)
+    {
+        $this->discountApplied = $discountApplied;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMonth()
+    {
+        return $this->month;
+    }
+
+    /**
+     * @param int $month
+     *
+     * @return $this
+     */
+    public function setMonth($month)
+    {
+        $this->month = $month;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int $year
+     *
+     * @return $this
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReceiptNumber()
+    {
+        $date = new \DateTime();
+        if ($this->getDate()) {
+            $date = $this->getDate();
+        }
+
+        return $date->format('Y').'/'.$this->getId();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSluggedReceiptNumber()
+    {
+        $date = new \DateTime();
+        if ($this->getDate()) {
+            $date = $this->getDate();
+        }
+
+        return $date->format('Y').'_'.$this->getId();
+    }
+
+    /**
+     * @return float
+     */
+    public function calculateTotalBaseAmount()
+    {
+        $result = 0.0;
+        /** @var InvoiceLine $line */
+        foreach ($this->lines as $line) {
+            $result = $result + $line->calculateBaseAmount();
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->id ? $this->getReceiptNumber().' · '.$this->getStudent().' · '.$this->getBaseAmountString() : '---';
+    }
+}
