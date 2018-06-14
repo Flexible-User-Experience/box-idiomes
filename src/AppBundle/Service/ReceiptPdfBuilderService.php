89a86ecd1fda3c0224243c767e71eb9e6336aea0
David Romaní <david@flux.cat>
2018-06-14 11:13:49 +0200

    improve receipt pdf builder 2

--- src/AppBundle/Service/ReceiptPdfBuilderService.php
+++ src/AppBundle/Service/ReceiptPdfBuilderService.php
@@ -186,9 +186,6 @@ class ReceiptPdfBuilderService
         $pdf->drawInvoiceLineSeparator($pdf->GetY());
         $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
 
-        // invoice table footer
-//        $pdf->MultiCell(135, $verticalTableGap, '-'.Invoice::TAX_IRPF.'% IRPF', 0, 'R', 0, 0, '', '', true, 0, false, true, 0, 'M');
-//        $pdf->MultiCell(15, $verticalTableGap, $this->floatMoneyFormat($invoice->getIrpf()), 0, 'R', 0, 1, '', '', true, 0, false, true, 0, 'M');
         $pdf->setFontStyle(null, 'B', 9);
         $pdf->MultiCell(135, $verticalTableGap, strtoupper($this->ts->trans('backend.admin.invoiceLine.total')), 0, 'R', 0, 0, '', '', true, 0, false, true, 0, 'M');
         $pdf->MultiCell(15, $verticalTableGap, $this->floatMoneyFormat($receipt->getBaseAmount()), 0, 'R', 0, 1, '', '', true, 0, false, true, 0, 'M');
