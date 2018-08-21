bc2216ab1711e60839de4ad76085a3eea0212bae
David Romaní <david@flux.cat>
2018-08-21 19:05:39 +0200

    add ROLE_MANAGER 1

--- src/AppBundle/Enum/UserRolesEnum.php
+++ src/AppBundle/Enum/UserRolesEnum.php
@@ -3,16 +3,17 @@
 namespace AppBundle\Enum;
 
 /**
- * UserRolesEnum class
+ * UserRolesEnum class.
  *
  * @category Enum
- * @package  AppBundle\Enum
+ *
  * @author   David Romaní <david@flux.cat>
  */
 class UserRolesEnum
 {
     const ROLE_USER = 'ROLE_USER';
     const ROLE_CMS = 'ROLE_CMS';
+    const ROLE_MANAGER = 'ROLE_MANAGER';
     const ROLE_ADMIN = 'ROLE_ADMIN';
     const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
 
@@ -24,6 +25,7 @@ class UserRolesEnum
         return array(
             self::ROLE_USER => 'backend.admin.user.user',
             self::ROLE_CMS => 'backend.admin.user.editor',
+            self::ROLE_MANAGER => 'backend.admin.user.manager',
             self::ROLE_ADMIN => 'backend.admin.user.admin',
             self::ROLE_SUPER_ADMIN => 'backend.admin.user.superadmin',
         );
