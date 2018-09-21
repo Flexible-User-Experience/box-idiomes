304d6097e68a7b9d959b3aa8d1044fcfcdf7f8cc
David Romaní <david@flux.cat>
2018-09-21 18:36:40 +0200

    build admin Student show view 4

---
+++ src/AppBundle/Resources/public/js/fullcalendar.student-settings.js
@@ -0,0 +1,56 @@
+jQuery(function () {
+    jQuery('#calendar-holder').fullCalendar({
+        header: {
+            left: 'prev today next',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay listWeek'
+        },
+        views: {
+            agendaWeek: {
+                allDaySlot: true,
+                slotLabelFormat: 'HH:mm',
+                scrollTime: '08:00:00',
+                minTime: '06:00:00',
+                maxTime: '22:00:00'
+            },
+            agendaDay: {
+                allDaySlot: true,
+                slotLabelFormat: 'HH:mm',
+                scrollTime: '08:00:00',
+                minTime: '06:00:00',
+                maxTime: '22:00:00'
+            }
+        },
+        height: 750,
+        locale: 'ca',
+        timeFormat: 'HH:mm',
+        firstDay: 1,
+        lazyFetching: false,
+        editable: true,
+        navLinks: true,
+        eventLimit: true,
+        businessHours: false,
+        displayEventTime: true,
+        fixedWeekCount: false,
+        weekNumbers: false,
+        defaultView: 'month',
+        themeSystem: 'bootstrap3',
+        googleCalendarApiKey: 'AIzaSyCZZYZV-LqX2qDtggiEo1GmeNhxe3SAhfI',
+        eventSources: [
+            {
+                googleCalendarId: 'es.spain#holiday@group.v.calendar.google.com',
+                backgroundColor: '#FED3D7',
+                textColor: '#FF0000',
+                color: '#FED3D7'
+            },
+            {
+                url: Routing.generate('ancarebeca_full_calendar_load'),
+                type: 'POST',
+                data: {},
+                error: function(data) {
+                    console.log('error!', data.responseText);
+                }
+            }
+        ]
+    });
+});
