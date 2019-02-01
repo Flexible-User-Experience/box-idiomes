3c5b38bc53e4a979132b1f6e2be4f1ee0ca9aba0
David Romaní <david@flux.cat>
2019-02-01 10:44:05 +0100

    add new tariffs enum

--- src/AppBundle/Enum/TariffTypeEnum.php
+++ src/AppBundle/Enum/TariffTypeEnum.php
@@ -15,6 +15,11 @@ class TariffTypeEnum
     const TARIFF_HALF_HOUR_PER_WEEK = 3;
     const TARIFF_PRIVATE_LESSON_PER_HOUR = 4;
     const TARIFF_SHARED_PRIVATE_LESSON_PER_HOUR = 5;
+    const TARIFF_THREE_AND_A_HALF_HOUR_PER_WEEK = 6;
+    const TARIFF_FOUR_HOUR_PER_WEEK = 7;
+    const TARIFF_FOUR_AND_A_HALF_HOUR_PER_WEEK = 8;
+    const TARIFF_FIVE_HOUR_PER_WEEK = 9;
+    const TARIFF_FIVE_AND_A_HALF_HOUR_PER_WEEK = 10;
 
     /**
      * @return array
@@ -28,6 +33,11 @@ class TariffTypeEnum
             self::TARIFF_HALF_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_4',
             self::TARIFF_PRIVATE_LESSON_PER_HOUR => 'backend.admin.tariff.tariff_5',
             self::TARIFF_SHARED_PRIVATE_LESSON_PER_HOUR => 'backend.admin.tariff.tariff_6',
+            self::TARIFF_THREE_AND_A_HALF_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_7',
+            self::TARIFF_FOUR_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_8',
+            self::TARIFF_FOUR_AND_A_HALF_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_9',
+            self::TARIFF_FIVE_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_10',
+            self::TARIFF_FIVE_AND_A_HALF_HOUR_PER_WEEK => 'backend.admin.tariff.tariff_11',
         );
     }
 
@@ -43,6 +53,11 @@ class TariffTypeEnum
             self::TARIFF_HALF_HOUR_PER_WEEK => '1,5h / setmana',
             self::TARIFF_PRIVATE_LESSON_PER_HOUR => 'hora particular',
             self::TARIFF_SHARED_PRIVATE_LESSON_PER_HOUR => 'hora particular compartida',
+            self::TARIFF_THREE_AND_A_HALF_HOUR_PER_WEEK => '3,5h / setmana',
+            self::TARIFF_FOUR_HOUR_PER_WEEK => '4h / setmana',
+            self::TARIFF_FOUR_AND_A_HALF_HOUR_PER_WEEK => '4,5h / setmana',
+            self::TARIFF_FIVE_HOUR_PER_WEEK => '5h / setmana',
+            self::TARIFF_FIVE_AND_A_HALF_HOUR_PER_WEEK => '5,5h / setmana',
         );
     }
 }
