60a17cd8d723b7e7071f974a8037f4d9ec6fa707
David Romaní <david@flux.cat>
2020-04-27 12:50:35 +0200

    enable /BackendFilesManagerMenuBuilder

---
+++ src/AppBundle/Menu/BackendFilesManagerMenuBuilder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class BackendFilesManagerMenuBuilder.
+ *
+ * @category Menu
+ */
+class BackendFilesManagerMenuBuilder
+{
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * Constructor.
+     *
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @param RequestStack $requestStack
+     *
+     * @return ItemInterface
+     */
+    public function createSideMenu(RequestStack $requestStack)
+    {
+        $route = $requestStack->getCurrentRequest()->get('_route');
+        $menu = $this->factory->createItem('Fitxers');
+        $menu
+            ->addChild(
+                'files',
+                array(
+                    'label' => 'backend.admin.files',
+                    'route' => 'file_manager',
+                    'route_parameters' => array(
+                        'conf' => 'default',
+                    ),
+                    'current' => 'file_manager' == $route || 'file_manager_rename' == $route || 'file_manager_upload' == $route,
+                )
+            )
+        ;
+
+        return $menu;
+    }
+}
